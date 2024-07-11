40137e1d925a38dcde829eac2f5c593add6e81bf
DESKTOP-S1E78EU\ASUS <chalisa.yod@techbiz.co.th>
2024-07-11 15:18:46 +0700

    add master position

---
+++ TechBiz-Microservices-Api/BusinessLogic/Identity/BizPositionManagement.cs
@@ -0,0 +1,242 @@
+﻿using DataLayer.Identitys;
+using Utilities;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Data;
+using Npgsql.Replication.PgOutput.Messages;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ExcelDataReader;
+using BusinessEntities.Identity;
+using BusinessEntities.HR.MasterModels;
+using DataLayer.HR.MasterModels;
+
+
+
+namespace BusinessLogic.Identity
+{
+    public class BizPositionManagement
+    {
+        private PositionRepository m_PositionRepository;
+
+        public BizPositionManagement()
+        {
+            m_PositionRepository = new PositionRepository();
+        }
+
+        public ResultMessage GetAllPosition()
+        {
+            int total = 0;
+            ResultMessage resultMessage = new ResultMessage();
+            DataTable dt = new DataTable();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(GlobalVariables.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    dt = m_PositionRepository.GetAll(conn);
+                    var data = dt.DataTableToList<tbm_permission>();
+                    resultMessage.status = true;
+                    resultMessage.code = GlobalMessage.SELECT_SUCCESS_CODE;
+                    resultMessage.data = data;
+                }
+                catch (Exception ex)
+                {
+                    resultMessage.description = ex.ToString();
+                    resultMessage.code = GlobalMessage.SELECT_ERROR_CODE;
+                    resultMessage.status = false;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                }
+
+
+
+            }//end using
+            return resultMessage;
+        }
+        public ResultMessage GetPositionById(int id)
+        {
+            int total = 0;
+            int qryRack = 0;
+
+            ResultMessage resultMessage = new ResultMessage();
+            DataTable dt = new DataTable();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(GlobalVariables.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    dt = m_PositionRepository.GetByKey(id, conn);
+                    var data = dt.DataTableToList<tbm_position>().FirstOrDefault();
+                    if (data is null)
+                    {
+                        throw new Exception("Data not found!");
+
+                    }
+
+                    resultMessage.status = true;
+                    resultMessage.code = GlobalMessage.SELECT_SUCCESS_CODE;
+                    resultMessage.data = data;
+
+                }
+                catch (Exception ex)
+                {
+                    resultMessage.description = ex.ToString();
+                    resultMessage.code = GlobalMessage.SELECT_ERROR_CODE;
+                    resultMessage.status = false;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                }
+
+            }//end using
+            return resultMessage;
+        }
+
+        public ResultMessage AddNewPosition(tbm_position model)
+        {
+            ResultMessage resultMessage = new ResultMessage();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(GlobalVariables.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    int id = m_PositionRepository.Insert(model, conn);
+                    model.position_id = id;
+
+                    resultMessage.data = model;
+                    resultMessage.code = GlobalMessage.INSERT_SUCCESS_CODE;
+                    resultMessage.status = true;
+                }
+                catch (Exception ex)
+                {
+                    resultMessage.description = ex.ToString();
+                    resultMessage.code = GlobalMessage.UPDATE_ERROR_CODE;
+                    resultMessage.status = false;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                }
+            }//end using
+
+
+            return resultMessage;
+        }
+
+        public ResultMessage UpdatePosition(tbm_position model)
+        {
+            ResultMessage resultMessage = new ResultMessage();
+            using (NpgsqlConnection conn = new NpgsqlConnection(GlobalVariables.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    int id = m_PositionRepository.Update(model, conn);
+                    model.position_id = id;
+
+                    resultMessage.data = model;
+                    resultMessage.code = GlobalMessage.UPDATE_SUCCESS_CODE;
+                    resultMessage.status = true;
+                }
+                catch (Exception ex)
+                {
+                    resultMessage.description = ex.ToString();
+                    resultMessage.code = GlobalMessage.UPDATE_ERROR_CODE;
+                    resultMessage.status = false;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                }
+            }//end using
+
+
+            return resultMessage;
+        }
+
+        public ResultMessage DeletePosition(int key)
+        {
+            ResultMessage resultMessage = new ResultMessage();
+            using (NpgsqlConnection conn = new NpgsqlConnection(GlobalVariables.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    int id = m_PositionRepository.Delete(key, conn);
+
+                    resultMessage.data = id;
+                    resultMessage.code = GlobalMessage.UPDATE_SUCCESS_CODE;
+                    resultMessage.status = true;
+                }
+                catch (Exception ex)
+                {
+                    resultMessage.description = ex.ToString();
+                    resultMessage.code = GlobalMessage.UPDATE_ERROR_CODE;
+                    resultMessage.status = false;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open) conn.Close();
+
+                }
+            }//end using
+
+
+            return resultMessage;
+        }
+
+        public ResultMessage GetAllPosition(QueryParameter queryParameter)
+        {
+            int total = 0;
+            ResultMessage resultMessage = new ResultMessage();
+            List<tbm_position> assyPartControlModel = new List<tbm_position>();
+            DataTable dt = new DataTable();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(GlobalVariables.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    dt = m_PositionRepository.GetAllPagination(queryParameter, out total, conn);
+
+                    var data = new { total = total, data = dt.DataTableToList<tbm_position>() };
+                    resultMessage.status = true;
+                    resultMessage.data = data;
+                }
+                catch (Exception ex)
+                {
+                    resultMessage.description = ex.ToString();
+                    resultMessage.code = GlobalMessage.UPDATE_ERROR_CODE;
+                    resultMessage.status = false;
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open)
+                        conn.Close();
+                }
+            }//using
+
+            return resultMessage;
+        }
+
+
+    }
+}
---
+++ TechBiz-Microservices-Api/DataLayer/Identitys/PositionRepository.cs
@@ -0,0 +1,224 @@
+﻿using BusinessEntities.Identity;
+using DataLayer.Core;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace DataLayer.Identitys
+{
+    public class PositionRepository : IDataRepository<tbm_position>
+    {
+        public int Delete(int Key, NpgsqlConnection conn, NpgsqlTransaction transaction = null)
+        {
+            int result = 0;
+            try
+            {
+                string sql = @"DELETE FROM authentication.tbm_position WHERE position_id = @position_id";
+
+                using (var cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.Add("@position_id", NpgsqlDbType.Integer).Value = Key;
+
+                    if (transaction != null)
+                    {
+                        cmd.Transaction = transaction;
+                    }
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public DataTable GetAll(NpgsqlConnection conn)
+        {
+            try
+            {
+                NpgsqlCommand sqlCommand = new NpgsqlCommand();
+                DataTable dataTable = new DataTable();
+
+                String select = @" SELECT * ";
+                String from = @" FROM  authentication.tbm_position  ";
+
+
+                sqlCommand.Connection = conn;
+
+                //get data
+                sqlCommand.CommandText = select + from;
+                NpgsqlDataReader reader = sqlCommand.ExecuteReader();
+                dataTable.Load(reader);
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public DataTable GetByKey(int Key, NpgsqlConnection conn)
+        {
+            try
+            {
+                NpgsqlCommand sqlCommand = new NpgsqlCommand();
+                DataTable dataTable = new DataTable();
+
+                String select = @" SELECT * ";
+                String from = @" FROM  authentication.tbm_position  ";
+                String where = @" WHERE  position_id = @key  ";
+
+                sqlCommand.Parameters.Add(new NpgsqlParameter("@key", NpgsqlDbType.Integer)).Value = Key;
+
+
+                sqlCommand.Connection = conn;
+
+                //get data
+                sqlCommand.CommandText = select + from + where;
+                NpgsqlDataReader reader = sqlCommand.ExecuteReader();
+                dataTable.Load(reader);
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public int Insert(tbm_position model, NpgsqlConnection conn, NpgsqlTransaction transaction = null)
+        {
+            int result = 0;
+            try
+            {
+                string sql = @"INSERT INTO authentication.tbm_position 											
+                                    (create_by,
+                                    status, 
+                                    dept_id, 
+                                    position_name) 											
+                                VALUES 											
+                                    ( @create_by, 
+                                    @status,
+                                    @dept_id, 
+                                    @position_name
+                                    ) RETURNING position_id;";
+
+                using (var cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.Add(new NpgsqlParameter("@create_by", NpgsqlDbType.Varchar) { Value = model.create_by });
+                    cmd.Parameters.Add(new NpgsqlParameter("@status", NpgsqlDbType.Varchar) { Value = model.status });
+                    cmd.Parameters.Add(new NpgsqlParameter("@dept_id", NpgsqlDbType.Bigint) { Value = model.dept_id });
+                    cmd.Parameters.Add(new NpgsqlParameter("@position_name", NpgsqlDbType.Varchar) { Value = model.position_name });
+                    
+                    if (transaction != null)
+                    {
+                        cmd.Transaction = transaction;
+                    }
+
+                    result = 0;
+                    int.TryParse(cmd.ExecuteScalar().ToString(), out result);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public int Update(tbm_position model, NpgsqlConnection conn, NpgsqlTransaction transaction = null)
+        {
+            int result = 0;
+            try
+            {
+                string sql = @"UPDATE authentication.tbm_position
+                       SET  update_by = @update_by,
+                            status = @status,
+                            dept_id = @dept_id,
+                            position_name = @position_name
+                       WHERE position_id = @position_id";
+
+                using (var cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.Add("@update_by", NpgsqlDbType.Varchar).Value = model.update_by;
+                    cmd.Parameters.Add("@status", NpgsqlDbType.Varchar).Value = model.status;
+                    cmd.Parameters.Add("@dept_id", NpgsqlDbType.Bigint).Value = model.dept_id;
+                    cmd.Parameters.Add("@position_name", NpgsqlDbType.Varchar).Value = model.position_name;
+                    cmd.Parameters.Add("@position_id", NpgsqlDbType.Bigint).Value = model.position_id;
+
+
+                    if (transaction != null)
+                    {
+                        cmd.Transaction = transaction;
+                    }
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public DataTable GetAllPagination(QueryParameter queryParameter, out int total, NpgsqlConnection conn)
+        {
+            try
+            {
+                NpgsqlCommand sqlCommand = new NpgsqlCommand();
+                DataTable dt = new DataTable();
+
+                string selectCount = @"SELECT count(1) ";
+                String select = @" SELECT * ";
+                String from = @"   FROM  authentication.tbm_position ";
+                String where = @" WHERE position_name ILIKE '%' || @searchValue || '%'
+                    OR status ILIKE '%' || @searchValue || '%'
+                    OR CAST(dept_id AS TEXT) ILIKE '%' || @searchValue || '%' ";
+                String orderBy = @" ORDER BY " + queryParameter.sortBy + " " + queryParameter.sortType + @"
+                              OFFSET (@page - 1) * @limit 
+                              FETCH NEXT @limit ROWS ONLY ";
+
+                if (queryParameter.searchValue == null || queryParameter.searchValue.Trim().Length == 0)
+                {
+                    where = "";
+                }
+                else
+                {
+                    sqlCommand.Parameters.Add(new NpgsqlParameter("@searchValue", NpgsqlDbType.Varchar)).Value = queryParameter.searchValue;
+                }
+
+                sqlCommand.Parameters.Add(new NpgsqlParameter("@page", NpgsqlDbType.Integer)).Value = queryParameter.page;
+                sqlCommand.Parameters.Add(new NpgsqlParameter("@limit", NpgsqlDbType.Integer)).Value = queryParameter.limit;
+
+
+                sqlCommand.Connection = conn;
+                //get total
+                sqlCommand.CommandText = selectCount + from + where;
+                total = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                Console.WriteLine(select + from + where + orderBy);
+
+                //get data
+                sqlCommand.CommandText = select + from + where + orderBy;
+                NpgsqlDataReader reader = sqlCommand.ExecuteReader();
+                dt.Load(reader);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
