6a60d8e3a09b93fb287537731dc463f436d2dbfb
Nat <147022709+chairatnat@users.noreply.github.com>
2024-07-30 12:07:34 +0700

    update

---
+++ TechBiz-Microservices-Api/BusinessEntities/HR/ProcessModels/FileName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities.HR.ProcessModels
+{
+    internal class FileName
+    {
+    }
+}
